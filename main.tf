c3e82e586ddfd21fde3e229204af65c54220cade
ajaychinku <kallapallyajaykumar@gmail.com>
2026-06-26 11:50:31 +0000

    compute_engine_has_been created

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
 provider "google" {
     project = "project-d89e9adb-fdf0-493c-ad4"
-    region  = "US-east1"
+    region = "us-central1"
   
+}
+resource "google_compute_instance" "ajay" {
+ name = "terraform-instance"
+ machine_type = "f1-micro"
+ zone = "us-central1-a"
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-11"
+   }
+ }
+ network_interface {
+   network = "default"
+   access_config {}
+ }
 }
