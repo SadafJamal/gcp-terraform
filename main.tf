397c339c64bd657180209baaecff67be2dee099e
ajaychinku <kallapallyajaykumar@gmail.com>
2026-06-26 12:15:21 +0000

    new_buckets_has_been_ created

--- main.tf
+++ main.tf
@@ -1,7 +1,32 @@
+
+
+terraform {
+ required_providers {
+   google = {
+     source = "hashicorp/google"
+     version = "~> 7.6"
+   }
+ }
+}
 provider "google" {
     project = "project-d89e9adb-fdf0-493c-ad4"
     region = "us-central1"
   
+}
+
+# Create a new storage bucket
+resource "google_storage_bucket" "static" {
+ name = "ajay26-storage-bucket"
+ location = "US"
+ storage_class = "STANDARD"
+ uniform_bucket_level_access = true
+}
+# Upload an object to the bucket
+resource "google_storage_bucket_object" "default" {
+ name = "sample_file.txt"
+ source = "${path.module}/sample_file.txt"
+ content_type = "text/plain"
+ bucket = google_storage_bucket.static.id
 }
 resource "google_compute_instance" "ajay" {
  name = "terraform-instance"
